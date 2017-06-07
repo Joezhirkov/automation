2bc384f9aa1fa3764a64e6fba88e5960eb6f4043
Joezhirkov <josephzhirkov@gmail.com>
2017-06-07 12:06:14 -0700

    Update Follow.java

--- Follow.java
+++ Follow.java
@@ -13,7 +13,7 @@ String url = "https://www.utest.com/";
 //Launch url
 driver.get(url);
 Thread.sleep(5000);
-//Verify that you are on the right page.
+//sign in
 String fail = "Test is failed!";
 String currenturl = driver.getCurrentUrl();
 System.out.println(currenturl);
@@ -29,27 +29,70 @@ if (currenttitle.equals(currenttitle)){
 } else {
 	System.out.println(fail);
 }
-//sign in.
 driver.findElement(By.linkText("Sign in")).click();
 Thread.sleep(5000);
 
 driver.findElement(By.xpath("/html/body/div/div/form/div[1]/input" )).clear();
-		                                          //Make sure you add and existing email
-driver.findElement(By.xpath("/html/body/div/div/form/div[1]/input" )).sendKeys("email");
+driver.findElement(By.xpath("/html/body/div/div/form/div[1]/input" )).sendKeys("EXAMPLE@gmail.com");
 driver.findElement(By.xpath("/html/body/div/div/form/div[2]/input")).clear();
-		                                         //Make sure you add a password that corresponds with ^^ email.
-driver.findElement(By.xpath("/html/body/div/div/form/div[2]/input")).sendKeys("Password");
+driver.findElement(By.xpath("/html/body/div/div/form/div[2]/input")).sendKeys("YOURPASSWORD");
 driver.findElement(By.xpath("/html/body/div/div/form/button")).click();
-Thread.sleep(5000);
+Thread.sleep(10000);
 //locate the follow button
-/*I am having issues with locating the follow button. It seems like it is created as a check box.
-please help! */
 
+driver.findElement(By.xpath("//label[contains(@for,'follow1')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow2')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow3')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow4')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow5')]")).click();
 
+for (int i = 0; i < 5; i++) {
+	 //click the next button
+	driver.findElement(By.xpath("//*[@id='mainContent']/div/div[1]/div/div/span[2]")).click();
 
-//click follow button 
-	}
+	 //wait 2 seconds
+	  Thread.sleep(1000);
+	 
 }
+
+Thread.sleep(2000);
+
+driver.findElement(By.xpath("//label[contains(@for,'follow6')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow7')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow8')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow9')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow10')]")).click();
+
+driver.navigate().to(driver.getCurrentUrl()); 
+Thread.sleep(5000);
+
+driver.findElement(By.xpath("//label[contains(@for,'follow1')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow2')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow3')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow4')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow5')]")).click();
+
+for (int i = 0; i < 5; i++) {
+	 //click the next button
+	driver.findElement(By.xpath("//*[@id='mainContent']/div/div[1]/div/div/span[2]")).click();
+
+	 //wait 2 seconds
+	  Thread.sleep(1000);
+	 
+}
+
+Thread.sleep(2000);
+
+driver.findElement(By.xpath("//label[contains(@for,'follow6')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow7')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow8')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow9')]")).click();
+driver.findElement(By.xpath("//label[contains(@for,'follow10')]")).click();
+
+driver.navigate().to(driver.getCurrentUrl()); 
+driver.close();
+
+
+
+	}}
 	
-
-
